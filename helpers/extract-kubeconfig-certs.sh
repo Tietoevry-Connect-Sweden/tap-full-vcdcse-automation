3ef20f681e095b5a9a843bd74d89b5ef2c5d5e87
pablete-im <pablete.im@gmail.com>
2022-11-02 12:04:26 +0100

    update helper to consider single cluster

--- helpers/extract-kubeconfig-certs.sh
+++ helpers/extract-kubeconfig-certs.sh
@@ -2,7 +2,7 @@
 
 CLUSTER_NAME_PREFIX=$1
 
-for PROFILE in view run iterate build 
+for PROFILE in full 
 do
 	echo "extracting certificates and keys from $i kubeconfig file"
 	cat kubeconfig${CLUSTER_NAME_PREFIX}${PROFILE}.txt | yq '.clusters[0].cluster.certificate-authority-data' | base64 -d > tap-${PROFILE}-cacert.pem
